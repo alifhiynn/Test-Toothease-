7eb37cfb2111bb2addfb7f4b012b07f96c248741
Asshh05 <natasyaazhaari05@gmail.com>
2025-06-21 00:04:27 +0800

    baiki coding

--- listappointment.php
+++ listappointment.php
@@ -20,6 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Appointment List</title>
+    <link rel="stylesheet" type="text\css" href="listappointment.css">
         
 </head>
 <body>
