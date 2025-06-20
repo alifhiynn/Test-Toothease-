505ef746fe860c70e0592b213349b385d3175c70
alifhiynn <alifahilyananorazmmi@gmail.com>
2025-06-20 08:36:36 +0800

    betulkan nama fail

--- logout.php
+++ logout.php
@@ -4,6 +4,6 @@ if (isset($_SESSION['username']))
 {
   $_SESSION = array();
   session_destroy();
-  echo "<meta http-equiv=\"refresh\" content=\"3;URL=index.php\">";
+  echo "<meta http-equiv=\"refresh\" content=\"3;URL= login.php\">";
 }
 ?>
